4c2ff16192975a8136b0ecf36e4093ab7ab3e775
Mai Thành Duy An <tiachop0102@gmail.com>
2018-06-21 02:16:55 +0700

    Create new Liferay 6.2 plugin project

---
+++ docroot/WEB-INF/src/com/minimart/portal/util/WebKeys.java
@@ -0,0 +1,6 @@
+package com.minimart.portal.util;
+
+public class WebKeys implements com.liferay.portal.kernel.util.WebKeys {
+	
+	public static final String MAT_HANG = "MAT_HANG";
+}
---
+++ docroot/html/minimart/portlet/init.jsp
@@ -0,0 +1,6 @@
+
+<%@ include file="/html/common/init.jsp" %>
+
+<portlet:defineObjects />
+
+<%@ include file="/html/minimart/portlet/init-ext.jsp" %>
