a310e45a8068065479c85e6e05bb1d42587dd575
Mai Thành Duy An <tiachop0102@gmail.com>
2018-06-21 09:24:43 +0700

    Create UnitTest for MatHang

---
+++ docroot/WEB-INF/test/unit/com/minimart/portlet/mathang/model/MatHangModelTestCase.java
@@ -0,0 +1,9 @@
+package com.minimart.portlet.mathang.model;
+
+public class MatHangModelTestCase {
+	protected MatHang addNew(){
+		MatHang matHang = null;
+		matHang.setCode("aasd");
+		return matHang;
+	}
+}
---
+++ docroot/WEB-INF/test/unit/com/minimart/portlet/mathang/model/MatHangTest.java
@@ -0,0 +1,31 @@
+package com.minimart.portlet.mathang.model;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.liferay.portal.kernel.dao.orm.FinderCacheUtil;
+
+/**
+ * @author Mai Thành Duy An
+ */
+
+public class MatHangTest extends MatHangModelTestCase {
+
+	@Before
+	public void setUp() throws Exception {
+		FinderCacheUtil.clearCache();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+
+	}
+
+	@Test
+	public void addNewInstant() throws Exception {
+
+	}
+}
